f297196675dfff616d37c6a4e07c452cc3fdbf7e
KyuNam <xmfpes@naver.com>
2017-10-29 14:45:24 +0900

    Feat(DispatcherServlet) : DispatcherServlet 생성

---
+++ src/main/java/next/controller/DispatcherServlet.java
@@ -0,0 +1,23 @@
+package next.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
+public class DispatcherServlet extends HttpServlet {
+	private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		log.debug("hello");
+	}
+}
