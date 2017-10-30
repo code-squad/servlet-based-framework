0beb367aacc8ee0ce7e12d77d6a7756fd01635ed
wheejuni <wheejuni@gmail.com>
2017-10-30 10:49:54 +0900

    refactor(created new exception NoUserFoundException)

--- src/main/java/core/mvc/controller/ProfileController.java
+++ src/main/java/core/mvc/controller/ProfileController.java
@@ -3,6 +3,7 @@ package core.mvc.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import core.db.DataBase;
+import core.db.exceptions.NoUserFoundException;
 import core.mvc.JspView;
 import core.mvc.ModelAndView;
 import next.model.User;
@@ -15,7 +16,7 @@ public class ProfileController implements Controller {
 		User user = DataBase.findUserById(userId);
 
 		if (user == null) {
-			throw new RuntimeException("No user matches with your request.");
+			throw new NoUserFoundException(NoUserFoundException.STD_NOUSER_ERROR_MSG);
 		}
 		ModelAndView mav = new ModelAndView(new JspView("/user/profile.jsp"));
 		mav.addObject("user", user);
