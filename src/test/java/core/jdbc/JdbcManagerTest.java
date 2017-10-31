1955ad8433a393bb23acbc652d0093c54ae16668
wheejuni <wheejuni@gmail.com>
2017-10-31 20:53:03 +0900

    refactor(remove unimplemented test methods)

--- src/test/java/core/jdbc/JdbcManagerTest.java
+++ src/test/java/core/jdbc/JdbcManagerTest.java
@@ -1,25 +1,5 @@
 package core.jdbc;
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import next.model.User;
-
 public class JdbcManagerTest {
 	
-	private User user;
-	
-	@Before
-	public void setup() {
-		this.user = new User("hi", "asdf", "asdf", "asdfasdfas");
-	}
-
-	@Test
-	public void test() {
-		
-		
-	}
-
 }
