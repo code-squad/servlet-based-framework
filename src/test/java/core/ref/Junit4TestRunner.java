73c1addcc48aa407435d2cddb68dfa7de3267972
wheejuni <wheejuni@gmail.com>
2017-11-03 16:39:54 +0900

    Reflection API 연습과제 구현

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,11 +1,25 @@
 package core.ref;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        
+        Arrays.asList(clazz.getDeclaredMethods()).stream()
+        .filter(m -> m.isAnnotationPresent(MyTest.class)).forEach(m -> {
+			try {
+				m.invoke(clazz.newInstance());
+			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException
+					| InstantiationException e) {
+				e.printStackTrace();
+			}
+		});
+        
 
     }
 }
--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -1,6 +1,12 @@
 package core.ref;
 
+import static org.junit.Assert.*;
+
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -15,6 +21,28 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+    }
+    
+    @Test
+    public void privateFieldAccess() {
+    		Student stu = new Student();
+    		Class<Student> parsedClass = Student.class;
+    		
+    		List<Field> fields = Arrays.asList(parsedClass.getDeclaredFields()).stream().collect(Collectors.toList());
+    		
+    		Field nameField = fields.stream().filter(f -> f.getName().equalsIgnoreCase("name"))
+    		.findFirst().get();
+    		
+    		nameField.setAccessible(true);
+    		try {
+				nameField.set(stu, "재성");
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    		logger.debug(stu.getName());
+    		assertEquals("재성", stu.getName());
+    		
     }
 
     @Test
