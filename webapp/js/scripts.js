907fd7a445bd151f3e17b0d07cb8474fa2ed3ff0
KyuNam <xmfpes@naver.com>
2017-11-06 16:53:32 +0900

    Refactor(js,query) - edit code(code convention)

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -15,41 +15,6 @@ $(document).ready(function(){/* jQuery toggle layout */
 			$(this).addClass("on");
 		}
 	});
-	function removeAnswer(e) {
-		e.preventDefault();
-		var deleteBtn = $(this);
-		var url = $(this).closest("form").attr("action");
-		var queryString = $(this).closest("form").serialize();
-		if (confirm("삭제하시겠습니까?")) {
-			$.ajax({
-				type : "POST",
-				url : url,
-				data : queryString,
-				dataType : "json",
-				error : function(data){
-					console.log(data);
-				},
-				success : function(data) {
-					console.log(data);
-					if (data.status) {
-						deleteBtn.closest("article").remove();
-					}
-				}
-			});
-		}
-	}
-	function addAnswer(e) {
-	    e.preventDefault(); //submit 이 자동으로 동작하는 것을 막는다.
-	    var queryString = $("form[name=answer]").serialize(); //form data들을 자동으로 묶어준다. 
-	    $.ajax({
-	    		type : "post",
-	        url : "/api/qna/addanswer",
-	        data : queryString,
-	        dataType : "json",
-	        error: onError,
-	        success : onSuccess
-        });
-	}
 	function onError(json, status){
 		console.log(json);
 	}
@@ -60,6 +25,41 @@ $(document).ready(function(){/* jQuery toggle layout */
 		$(".qna-comment-slipp-articles").prepend(template);
 		$("input[name=writer]").val("");
 		$("textarea[name=contents]").val("");
+	}
+	function removeAnswer(e) {
+		e.preventDefault();
+		var deleteBtn = $(this);
+		var url = $(this).closest("form").attr("action");
+		var queryString = $(this).closest("form").serialize();
+		if (confirm("삭제하시겠습니까?")) {
+			$.ajax({
+				type : "POST",
+				url,
+				data : queryString,
+				dataType : "json",
+				error(data){
+					console.log(data);
+				},
+				success(data) {
+					Console.log(data);
+					if (data.status) {
+						deleteBtn.closest("article").remove();
+					}
+				}
+			});
+		}
+	}
+	function addAnswer(e) {
+		e.preventDefault();//submit 이 자동으로 동작하는 것을 막는다.
+		var queryString = $("form[name=answer]").serialize();//form data들을 자동으로 묶어준다.
+		$.ajax({
+			type : "post",
+			url : "/api/qna/addanswer",
+			data : queryString,
+			dataType : "json",
+			error: onError,
+			success : onSuccess
+        });
 	}
 	$(".form-delete").click(removeAnswer);
 	$(".answerWrite input[type=submit]").click(addAnswer);
