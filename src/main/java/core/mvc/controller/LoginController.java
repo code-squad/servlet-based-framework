9773fdf38d6b88285915d1b408513336417bb2f7
wheejuni <wheejuni@gmail.com>
2017-11-06 14:56:25 +0900

    refactor(convert to annotation-based controller)

--- src/main/java/core/mvc/controller/LoginController.java
+++ src/main/java/core/mvc/controller/LoginController.java
@@ -2,13 +2,17 @@ package core.mvc.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import core.annotation.Controller;
+import core.annotation.RequestMapping;
 import core.mvc.JspView;
 import core.mvc.ModelAndView;
 
+@Controller
 public class LoginController implements LegacyControllerInterface{
 
 
 	@Override
+	@RequestMapping("/users/loginForm")
 	public ModelAndView run(HttpServletRequest req) {
 		return new ModelAndView(new JspView("/user/login.jsp"));
 	}
