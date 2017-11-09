e757d729ebde5fb87405e144dd6f75fbe2c50cf3
KyuNam <xmfpes@naver.com>
2017-11-09 17:47:05 +0900

    Refactor(ControllerScanner) - Exception, Stream 문법 적용

--- src/main/java/core/annotation/scan/ControllerScanner.java
+++ src/main/java/core/annotation/scan/ControllerScanner.java
@@ -2,7 +2,9 @@ package core.annotation.scan;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -15,20 +17,24 @@ import core.exception.ControllerInstantiationException;
 public class ControllerScanner {
 	private static final Logger logger = LoggerFactory.getLogger(ControllerScanner.class);
 	public Map<Class<?>, Object> getController(){
-		Reflections reflections = new Reflections("next.controller");
-		Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(Controller.class);
+		Reflections reflections = getReflections("next.controller");
+		Set<Class<?>> annotated = getAnnotated(reflections);
 		return InstantiateControllers(annotated);
 	}
+	
 	public Map<Class<?>, Object> InstantiateControllers(Set<Class<?>> annotated) {
-		Map<Class<?>, Object> controllerMap = new HashMap<Class<?>, Object>();
-		annotated.stream().forEach(a -> {
-			try {
-				controllerMap.put(a, a.newInstance());
-			} catch (Exception e) {
+		return annotated.stream().collect(Collectors.toMap(a -> a, a -> {
+			try { return a.newInstance(); } catch (Exception e) {
 				throw new ControllerInstantiationException();
 			}
-		});
-		logger.debug("instanitate complete");
-		return controllerMap;
+		}));
+	}
+	
+	public Reflections getReflections(String pacakge) {
+		return new Reflections("next.controller");
+	}
+	
+	public Set<Class<?>> getAnnotated(Reflections reflections){
+		return reflections.getTypesAnnotatedWith(Controller.class);
 	}
 }
