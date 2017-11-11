ad4945beb24b87fde46cd4ec8665dbdb952fda3e
KyuNam <xmfpes@naver.com>
2017-11-11 21:17:17 +0900

    Refactor(Codacy) - edit codacy issues

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,6 +1,5 @@
 package core.ref;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -2,7 +2,6 @@ package core.ref;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
