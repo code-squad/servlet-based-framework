55f79ad2a5649ef279490c880626cedfdf31492b
abcshc <thrrk@naver.com>
2018-01-12 11:04:45 +0900

    refactor: ModelAndView

---
+++ src/main/java/next/controller/Controller.java
@@ -0,0 +1,11 @@
+package next.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import core.mvc.ModelAndView;
+
+public interface Controller {
+	ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
+
+}
