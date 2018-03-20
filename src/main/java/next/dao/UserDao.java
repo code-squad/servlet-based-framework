56c1e0aadec23350cd0db32bea6a8db79d260d3d
Jiwon Bae <jwb8705@gmail.com>
2018-03-20 23:37:24 +0900

    Implements codes about find user list from database and update user info that is in database

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -15,14 +15,17 @@ public class UserDao {
         Connection con = null;
         PreparedStatement pstmt = null;
         try {
+            // 커넥션 객체 생성
             con = ConnectionManager.getConnection();
             String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+            // Creates a PreparedStatement object for sending
+            // parameterized SQL statements to the database.
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, user.getUserId());
             pstmt.setString(2, user.getPassword());
             pstmt.setString(3, user.getName());
             pstmt.setString(4, user.getEmail());
-
+            // implement spl statement
             pstmt.executeUpdate();
         } finally {
             if (pstmt != null) {
@@ -37,11 +40,51 @@ public class UserDao {
 
     public void update(User user) throws SQLException {
         // TODO 구현 필요함.
+        Connection dbConn = null;
+        PreparedStatement pstmt = null;
+        try {
+            dbConn = ConnectionManager.getConnection();
+            String sql = "UPDATE USERS set password = ?, name = ?, email = ? WHERE userId = ?";
+            pstmt = dbConn.prepareStatement(sql);
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getUserId());
+            pstmt.executeUpdate();
+        } finally {
+            if(pstmt != null){
+                pstmt.close();
+            }
+            if(dbConn != null){
+                dbConn.close();
+            }
+        }
     }
 
     public List<User> findAll() throws SQLException {
         // TODO 구현 필요함.
-        return new ArrayList<User>();
+        Connection dbConn= null;
+        PreparedStatement pstmt = null;
+        List<User> users = new ArrayList<>();
+        try {
+            dbConn = ConnectionManager.getConnection();
+            String sql = "SELECT userId, password, name, email FROM USERS";
+            pstmt = dbConn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()){
+                User user = new User(rs.getString("userId"), rs.getString(2), rs.getString(3), rs.getString(4));
+                users.add(user);
+            }
+        }finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (dbConn != null) {
+                dbConn.close();
+            }
+        }
+        return users;
     }
 
     public User findByUserId(String userId) throws SQLException {
@@ -61,8 +104,8 @@ public class UserDao {
                 user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
                         rs.getString("email"));
             }
-
             return user;
+
         } finally {
             if (rs != null) {
                 rs.close();
